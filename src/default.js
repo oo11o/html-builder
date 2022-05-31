e7acb93aa64149c6ce27b18e10d17da354c2d5a0
oo11o <ymyr.vlad@gmail.com>
2022-05-31 21:00:56 +0300

    feat: add default virtual html

---
+++ src/default.js
@@ -0,0 +1,41 @@
+export default {
+  tag: "html",
+  attribute: [{"lang": "en"}],
+  content: "",
+  children: [
+    {
+      tag: "head",
+      attribute: [],
+      content: "",
+      children: [
+        {
+          tag: "meta",
+          attribute: [{name: 'charset', value: 'UTF-8'}],
+          content: "",
+          children: []
+        },
+        {
+          tag: "meta",
+          attribute: [
+            {name: 'name', value: 'viewport'},
+            {name: 'content', value: 'width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0'},
+          ],
+          content: "",
+          children: []
+        },
+        {
+          tag: "title",
+          attribute: [],
+          content: "Title page",
+          children: []
+        }
+      ]
+    },
+    {
+      tag: "body",
+      attribute: [],
+      content: "",
+      children: []
+    }
+  ]
+};
