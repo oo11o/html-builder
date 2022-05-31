3994e3b788594df31d4b54228a60531950303c38
oo11o <ymyr.vlad@gmail.com>
2022-05-31 21:11:05 +0300

    style: eslint correct style

--- src/default.js
+++ src/default.js
@@ -1,41 +1,41 @@
 export default {
-  tag: "html",
-  attribute: [{"lang": "en"}],
-  content: "",
+  tag: 'html',
+  attribute: [{ lang: 'en' }],
+  content: '',
   children: [
     {
-      tag: "head",
+      tag: 'head',
       attribute: [],
-      content: "",
+      content: '',
       children: [
         {
-          tag: "meta",
-          attribute: [{name: 'charset', value: 'UTF-8'}],
-          content: "",
-          children: []
+          tag: 'meta',
+          attribute: [{ name: 'charset', value: 'UTF-8' }],
+          content: '',
+          children: [],
         },
         {
-          tag: "meta",
+          tag: 'meta',
           attribute: [
-            {name: 'name', value: 'viewport'},
-            {name: 'content', value: 'width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0'},
+            { name: 'name', value: 'viewport' },
+            { name: 'content', value: 'width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0' },
           ],
-          content: "",
-          children: []
+          content: '',
+          children: [],
         },
         {
-          tag: "title",
+          tag: 'title',
           attribute: [],
-          content: "Title page",
-          children: []
-        }
-      ]
+          content: 'Title page',
+          children: [],
+        },
+      ],
     },
     {
-      tag: "body",
+      tag: 'body',
       attribute: [],
-      content: "",
-      children: []
-    }
-  ]
+      content: '',
+      children: [],
+    },
+  ],
 };
--- test/virtualhtm.test.js
+++ test/virtualhtm.test.js
@@ -1,24 +1,24 @@
-import VirtualHTML from "../src/VirtualHTML.js";
+import VirtualHTML from '../src/VirtualHTML';
 
 const virtualHTML = new VirtualHTML();
 
-describe('createTag(): convert data set to html string',() => {
+describe('createTag(): convert data set to html string', () => {
   test('get html tag without attributes', () => {
-    expect(virtualHTML.createTag({tag:'html'})).toEqual('<html></html>');
-    expect(virtualHTML.createTag({tag:'div'})).toEqual('<div></div>');
-    expect(virtualHTML.createTag({tag:'div', attributes: []})).toEqual('<div></div>');
+    expect(virtualHTML.createTag({ tag: 'html' })).toEqual('<html></html>');
+    expect(virtualHTML.createTag({ tag: 'div' })).toEqual('<div></div>');
+    expect(virtualHTML.createTag({ tag: 'div', attributes: [] })).toEqual('<div></div>');
   });
   test('get html tag with attributes', () => {
     const elementHtmlWithAttributes = {
       tag: 'html',
       children: [],
       attributes: [
-        {name: 'lang', value: 'ru'},
-        {name: 'empty', value: ''},
-        {name: 'class', value: 'container'},
+        { name: 'lang', value: 'ru' },
+        { name: 'empty', value: '' },
+        { name: 'class', value: 'container' },
       ],
     };
     expect(virtualHTML.createTag(elementHtmlWithAttributes))
-        .toEqual('<html lang="ru" empty="" class="container"></html>')
-  })
+      .toEqual('<html lang="ru" empty="" class="container"></html>');
+  });
 });
