3ea0d8148e8837f41b94b5b7baa0eec3d4e4566c
oo11o <ymyr.vlad@gmail.com>
2022-06-01 20:04:20 +0300

    test: fix test method createTag

--- test/virtualhtm.test.js
+++ test/virtualhtm.test.js
@@ -1,24 +1,43 @@
 import VirtualHTML from '../src/VirtualHTML';
 
 const virtualHTML = new VirtualHTML();
+const emptyParameters = {attributes: [], content: '', children: []}
 
 describe('createTag(): convert data set to html string', () => {
-  test('get html tag without attributes', () => {
-    expect(virtualHTML.createTag({ tag: 'html' })).toEqual('<html></html>');
-    expect(virtualHTML.createTag({ tag: 'div' })).toEqual('<div></div>');
-    expect(virtualHTML.createTag({ tag: 'div', attributes: [] })).toEqual('<div></div>');
+  test('get tag without attributes', () => {
+    expect(virtualHTML.createTag({tag: 'html', ...emptyParameters}))
+        .toEqual({startTag: '<html>', closedTag: '</html>'});
+    expect(virtualHTML.createTag({tag: 'div', ...emptyParameters}))
+        .toEqual({startTag: '<div>', closedTag: '</div>'});
   });
-  test('get html tag with attributes', () => {
+
+  test('get tag self-closing tag', () => {
+    expect(virtualHTML.createTag({tag: 'hr', ...emptyParameters}))
+        .toEqual({startTag: '<hr>', closedTag: ''});
+    expect(virtualHTML.createTag({tag: 'img', ...emptyParameters}))
+        .toEqual({startTag: '<img>', closedTag: ''});
+  });
+
+  test('get tag with attributes and content', () => {
     const elementHtmlWithAttributes = {
       tag: 'html',
       children: [],
+      content: [],
       attributes: [
-        { name: 'lang', value: 'ru' },
-        { name: 'empty', value: '' },
-        { name: 'class', value: 'container' },
+        {name: 'lang', value: 'ru'},
+        {name: 'empty', value: ''},
+        {name: 'class', value: 'container'},
       ],
     };
     expect(virtualHTML.createTag(elementHtmlWithAttributes))
-      .toEqual('<html lang="ru" empty="" class="container"></html>');
+        .toEqual({startTag: '<html lang="ru" empty="" class="container">', closedTag: '</html>'});
+    elementHtmlWithAttributes.tag = 'hr';
+    expect(virtualHTML.createTag(elementHtmlWithAttributes))
+        .toEqual({startTag: '<hr lang="ru" empty="" class="container">', closedTag: ''});
+    elementHtmlWithAttributes.tag = 'p';
+    elementHtmlWithAttributes.content  = 'This some text';
+    elementHtmlWithAttributes.class = 'container red mg-5';
+    expect(virtualHTML.createTag(elementHtmlWithAttributes))
+        .toEqual({startTag: '<p lang="ru" empty="" class="container">', closedTag: '</p>'});
   });
-});
+})
